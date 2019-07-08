4b9fc0bfcd0e7813d77c68f7a8453021ae21e3cb
Sandip <sandiproy10@yahoo.com>
2019-07-08 17:57:02 +0800

    Create Bubble Sort.js

---
+++ Sorting algorithm using JS/Bubble Sort.js
@@ -0,0 +1,23 @@
+ //implementation of bubble sort algorithm 
+ 
+function bubbleSort(arr){
+  for (let i=0; i<arr.length-1; i++){
+    for (let j=0; j<arr.length-1-i; j++){
+      if(arr[j]>arr[j+1]){
+        swap(arr,j,j+1)
+      }
+    }
+  }
+  return arr;
+}
+
+function swap(arr,n,m){
+  if (arr[n]>arr[m]){
+    let temp = arr[n];
+    arr[n] = arr[m];
+    arr[m] = temp;
+  }
+  return arr;
+}
+
+bubbleSort([2,5,13,1,4,16,5,23,3]);
