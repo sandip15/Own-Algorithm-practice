41c5ec0febb00581bc12cbf6e67f8305bf9b688f
Sandip <sandiproy10@yahoo.com>
2019-07-12 18:18:01 +0800

    Create fibonacci number.js
    find the fibonacci number of the given index(n)

---
+++ Recursion/fibonacci number.js
@@ -0,0 +1,26 @@
+// fibonacci number sequence : 0,1,1,2,3,5,8,13,21,34,55,89,144......
+//find the fibonacci number of the given index(n)
+
+
+function fibonacciIterative(n){
+  //iterative solution
+  let arr =[0,1];
+  for (let i=2 ;i<=n; i++){
+    arr.push(arr[i-2]+arr[i-1]);
+  }
+  result = 0;
+  for (let i=1; i<n ;i++){
+    result = (n-1) + (n-2)
+  }
+  return arr[n];
+}
+function fibonacciRecursion(n){
+  //recursion solution
+  if (n<2){
+    return n;
+  }
+  return fibonacciRecursion(n-1)+ fibonacciRecursion(n-2);
+}
+
+fibonacciIterative(6)
+fibonacciRecursion(6)
