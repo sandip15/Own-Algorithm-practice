3425fc3d78da871a08f3103bf081caec3b6a7dc5
Sandip <sandiproy10@yahoo.com>
2020-03-18 20:05:24 +0600

    Update factorial of a number.js

--- Recursion/factorial of a number.js
+++ Recursion/factorial of a number.js
@@ -1,8 +1,8 @@
 //Question: implement factorial of a number by both recursive and iterative way
-/*generally by recursive way the space complexity is bit high due to adding of multiple stacks 
+/* generally by recursive way the space complexity is bit high due to adding of multiple stacks 
 (created by multiple function calls)on the top of other and not releasing those stacks, and vice versa */
 
-function factorialIterative(number){
+function factorialIterative(number){  // more time complexity but less space complexity comparing with the recursive approach
   //solution using interative approach （ generally slower than the recursive approach)
   let answer = 1;
   for (let i=1; i<= number ; i++){
